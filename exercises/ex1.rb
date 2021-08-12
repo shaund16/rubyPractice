a0866497abd92d3b5f66b3b562c5e35fbb237075
Shaun <s-purslow@hotmail.com>
2021-08-12 00:02:29 +0000

    make exercise folder and put files in

---
+++ exercises/ex1.rb
@@ -0,0 +1,9 @@
+# A Good First Program
+
+puts "Hello world!"
+puts "Hello Again " 
+puts "Hello I like typing this"
+puts "This is fun"
+puts "Yay!, Printing"
+puts "Id rather you not"
+puts "I said do not touch this"
---
+++ exercises/ex28.rb
@@ -0,0 +1,14 @@
+# Boolean Practice
+
+true && true #true
+false && true #false
+1 == 1 && 2 == 1 #false
+"test" == "test" #true
+1 == 1 || 2 != 1 #true
+true || 1 == 1 #true
+"test" == "testing" #false
+1 != 0 && 2 == 1 #false
+"test" != "testing" #true
+"test" == 1 #false
+!(true && false) #true
+!(1 == 1 && 0 != 1) #false
