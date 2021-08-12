1b884735052c74e9a26970306c19a6b94ac5fe7a
Shaun <s-purslow@hotmail.com>
2021-08-12 00:00:35 +0000

    upload files and complete exercises

---
+++ ex2.rb
@@ -0,0 +1,7 @@
+# A comment in Ruby
+
+puts "I could have code like this"
+
+# This code will not run 
+
+puts "I will run"
---
+++ ex33.rb
@@ -0,0 +1,20 @@
+#While Loops
+
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+
+  puts "-----------------------"
+end
+
+puts "The numbers: "
+
+# remember you can write this 2 other ways?
+numbers.each {|num| puts num }
